a7250bd002595a36b4730cb51be7b6c8dbd26ab6
Foresteam <heler1141@gmail.com>
2021-11-05 10:44:59 +0300

    Process...

--- Assets/Scripts/Abstract/Item.cs
+++ Assets/Scripts/Abstract/Item.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item {
-    public WordItem WorldRep { get; private set; }
+    public WorldItem WorldRep { get; private set; }
     public Sprite UIRep { get; private set; }
+    private Sprite _uiSprite;
 
-    public Item(WordItem worldRepresentation) {
+    public Item(WorldItem worldRepresentation) {
         WorldRep = worldRepresentation;
-        UIRep = worldRepresentation.uiRepresentationPrefab;
+        _uiSprite = worldRepresentation.GetComponent<SpriteRenderer>().sprite;
+    }
+    public void DoPickUp() {
+        WorldRep.gameObject.SetActive(false);
+    }
+    public void DoDrop(Vector2 playerPosition) {
+        WorldRep.transform.position = playerPosition + new Vector2(0, 1);
+        WorldRep.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.value * 5, Random.value * 5);
+        WorldRep.gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/WorldItem.cs
+++ Assets/Scripts/WorldItem.cs
@@ -2,6 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WordItem : MonoBehaviour {
-    public Sprite uiRepresentationPrefab;
+public class WorldItem : MonoBehaviour {
 }
