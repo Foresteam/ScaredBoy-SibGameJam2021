c8e6373e13c2ee7a7543f7d4c8185e558118a4e9
Foresteam <heler1141@gmail.com>
2021-11-07 15:38:25 +0300

    Fixed dropping. Inventory can now be filled on start

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,53 +1,55 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-	class Inventory : MonoBehaviour {
-		[SerializeField] private Color _frameColorActive = Color.yellow;
-		[SerializeField] private Color _frameColorInactive = Color.white;
-		[SerializeField] private int _size = 3;
-		public int size { get => _size; }
-		public delegate void OnSelectedItemChangedHandler(object sender, WorldItem item);
-		public event OnSelectedItemChangedHandler OnSelectedItemChange;
+class Inventory : MonoBehaviour {
+	[SerializeField] private Color _frameColorActive = Color.yellow;
+	[SerializeField] private Color _frameColorInactive = Color.white;
+	[SerializeField] private WorldItem[] _itemsToAdd;
+	[SerializeField] private int _size = 3;
+	public int size { get => _size; }
+	public delegate void OnSelectedItemChangedHandler(object sender, WorldItem item);
+	public event OnSelectedItemChangedHandler OnSelectedItemChange;
 
-		private List<InventorySlot> _slots;
-		private int _activeSlot;
+	private List<InventorySlot> _slots;
+	private int _activeSlot;
 
-		void Start() {
-			_activeSlot = -1;
-			_slots = new List<InventorySlot>(FindObjectOfType<GUICanvas>().GetComponentInChildren<GUIItems>().GetComponentsInChildren<InventorySlot>());
-			_slots.Sort((InventorySlot a, InventorySlot b) => a.gameObject.name.CompareTo(b.gameObject.name));
-			SelectItem(0);
-		}
-		public void SelectItem(int slot) {
-			if (_activeSlot >= 0)
-				_slots[_activeSlot].PaintFrame(_frameColorInactive);
-			_slots[slot].PaintFrame(_frameColorActive);
-			_activeSlot = slot;
-			OnSelectedItemChange?.Invoke(this, _slots[_activeSlot].worldItem);
-		}
-		public void PickupItem(WorldItem worldItem, Vector2 playerPosition) {
-			int slot;
-			if (!_slots[_activeSlot].IsEmpty()) {
-				for (slot = 0; slot < size; slot++)
-					if (_slots[slot].IsEmpty())
-						break;
-				if (slot == size) {
-					slot = _activeSlot;
-					DropItem(playerPosition);
-				}
-			}
-			else
-				slot = _activeSlot;
-			_slots[slot].DoPickup(worldItem);
-			if (slot == _activeSlot)
-				OnSelectedItemChange?.Invoke(this, worldItem);
-		}
-		public void DropItem(Vector2 playerPosition) {
-			if (_slots[_activeSlot].IsEmpty())
-				return;
-			_slots[_activeSlot].DoDrop(playerPosition);
-			OnSelectedItemChange?.Invoke(this, null);
-		}
+	void Start() {
+		_activeSlot = -1;
+		_slots = new List<InventorySlot>(FindObjectOfType<GUICanvas>().GetComponentInChildren<GUIItems>().GetComponentsInChildren<InventorySlot>());
+		_slots.Sort((InventorySlot a, InventorySlot b) => a.gameObject.name.CompareTo(b.gameObject.name));
+		SelectItem(0);
+
+		foreach (WorldItem item in _itemsToAdd)
+			PickupItem(item, transform.position);
 	}
-
-
+	public void SelectItem(int slot) {
+		if (_activeSlot >= 0)
+			_slots[_activeSlot].PaintFrame(_frameColorInactive);
+		_slots[slot].PaintFrame(_frameColorActive);
+		_activeSlot = slot;
+		OnSelectedItemChange?.Invoke(this, _slots[_activeSlot].worldItem);
+	}
+	public void PickupItem(WorldItem worldItem, Vector2 playerPosition) {
+		int slot;
+		if (!_slots[_activeSlot].IsEmpty()) {
+			for (slot = 0; slot < size; slot++)
+				if (_slots[slot].IsEmpty())
+					break;
+			if (slot == size) {
+				slot = _activeSlot;
+				DropItem(playerPosition);
+			}
+		}
+		else
+			slot = _activeSlot;
+		_slots[slot].DoPickup(worldItem);
+		if (slot == _activeSlot)
+			OnSelectedItemChange?.Invoke(this, worldItem);
+	}
+	public void DropItem(Vector2 playerPosition) {
+		if (_slots[_activeSlot].IsEmpty())
+			return;
+		_slots[_activeSlot].DoDrop(playerPosition);
+		OnSelectedItemChange?.Invoke(this, null);
+	}
+}
--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -4,45 +4,46 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 
-	public class InventorySlot : MonoBehaviour {
-		[SerializeField] GameObject _uiItemPrefab;
-		private GameObject _uiItem;
-		private WorldItem _worldItem;
-		private Sprite _uiSprite;
+public class InventorySlot : MonoBehaviour {
+	[SerializeField] GameObject _uiItemPrefab;
+	private GameObject _uiItem;
+	private WorldItem _worldItem;
+	private Sprite _uiSprite;
 
-		public WorldItem worldItem { get => _worldItem; }
+	public WorldItem worldItem { get => _worldItem; }
 
-        private void Start() {
-			_uiItem = null;
-			_worldItem = null;
-			_uiSprite = null;
-        }
-
-        public void DoPickup(WorldItem worldItem) {
-			_worldItem = worldItem;
-			_uiSprite = _worldItem.GetComponent<SpriteRenderer>().sprite;
-
-			_uiItem = GameObject.Instantiate(_uiItemPrefab, transform);
-			_uiItem.GetComponent<Image>().sprite = _uiSprite;
-			var rt = _uiItem.GetComponent<RectTransform>();
-			rt.sizeDelta = GetComponent<RectTransform>().sizeDelta;
-
-			_worldItem.gameObject.SetActive(false); // deactivate the world representation, leaving the UI one only
-		}
-
-		public void DoDrop(Vector2 playerPosition) {
-			_worldItem.gameObject.SetActive(true);
-			_worldItem.GetComponent<Rigidbody2D>().isKinematic = false;
-			_worldItem.transform.position = playerPosition + new Vector2(0, 1);
-			_worldItem.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -5);
-			Object.Destroy(_uiItem);
-
-			_uiItem = null;
-			_worldItem = null;
-			_uiSprite = null;
-		}
-		public void PaintFrame(Color color) {
-			GetComponent<Image>().color = color;
-		}
-		public bool IsEmpty() => _worldItem == null || _worldItem == null || _uiSprite == null;
+	private void Start() {
+		_uiItem = null;
+		_worldItem = null;
+		_uiSprite = null;
 	}
+
+	public void DoPickup(WorldItem worldItem) {
+		_worldItem = worldItem;
+		_uiSprite = _worldItem.GetComponent<SpriteRenderer>().sprite;
+
+		_uiItem = GameObject.Instantiate(_uiItemPrefab, transform);
+		_uiItem.GetComponent<Image>().sprite = _uiSprite;
+		var rt = _uiItem.GetComponent<RectTransform>();
+		rt.sizeDelta = GetComponent<RectTransform>().sizeDelta;
+
+		_worldItem.gameObject.SetActive(false); // deactivate the world representation, leaving the UI one only
+	}
+
+	public void DoDrop(Vector2 playerPosition) {
+		_worldItem.gameObject.SetActive(true);
+		_worldItem.GetComponent<Rigidbody2D>().isKinematic = false;
+		_worldItem.transform.position = playerPosition + new Vector2(0, 1);
+		_worldItem.transform.rotation = new Quaternion();
+		_worldItem.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -5);
+		Object.Destroy(_uiItem);
+
+		_uiItem = null;
+		_worldItem = null;
+		_uiSprite = null;
+	}
+	public void PaintFrame(Color color) {
+		GetComponent<Image>().color = color;
+	}
+	public bool IsEmpty() => _worldItem == null || _worldItem == null || _uiSprite == null;
+}
