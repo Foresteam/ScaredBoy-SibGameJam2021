84614e6d6b52e70c31c0086a520a4ba829033085
Foresteam <heler1141@gmail.com>
2021-11-07 12:49:55 +0300

    Коряга done

--- Assets/Scripts/FantomObject.cs
+++ Assets/Scripts/FantomObject.cs
@@ -6,22 +6,24 @@ public class FantomObject : MonoBehaviour
 {
     public GameObject Fantom;
     public GameObject RealObject;
+    private bool _setReal;
     void Start()
     {
+        _setReal = false;
         Fantom.SetActive(true);
         RealObject.SetActive(false);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 6)
-        {
+        Flashlight _;
+        if (!_setReal && collision.gameObject.TryGetComponent<Flashlight>(out _))
             SetRealObject();
-        }
     }
     public void SetRealObject()
     {
         Fantom.SetActive(false);
         RealObject.SetActive(true);
+        _setReal = true;
     }
 }
